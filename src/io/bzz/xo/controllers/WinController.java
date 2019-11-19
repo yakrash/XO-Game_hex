100c97d55137d3902d34a2f3273d493cb35df19e
yakrash <yakrash@ya.ru>
2019-11-19 18:49:07 +0300

    WinController - created without code

---
+++ src/io/bzz/xo/controllers/WinController.java
@@ -0,0 +1,18 @@
+package io.bzz.xo.controllers;
+
+import io.bzz.xo.model.Field;
+import io.bzz.xo.model.Figure;
+
+import java.awt.*;
+
+public class WinController {
+
+    public Figure getWinner (Field firld){
+
+    }
+
+    public Figure check (Field field, Point point){
+
+    }
+
+}
