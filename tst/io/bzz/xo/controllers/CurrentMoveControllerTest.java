023283412dc93edb4566b4d91f639569544482d8
yakrash <yakrash@ya.ru>
2019-11-19 18:48:22 +0300

    CurrentMoveControllerTest - created without code

---
+++ tst/io/bzz/xo/controllers/CurrentMoveControllerTest.java
@@ -0,0 +1,15 @@
+package io.bzz.xo.controllers;
+
+import io.bzz.xo.model.Field;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CurrentMoveControllerTest {
+
+    @Test
+    public void currentMove() {
+        Field field = new Field();
+
+    }
+}
