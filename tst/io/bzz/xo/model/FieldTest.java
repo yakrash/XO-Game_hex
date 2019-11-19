dfda76e7a43d1e9906fa466ceb1c44a4f87165a0
yakrash <yakrash@ya.ru>
2019-11-19 15:29:46 +0300

    FieldTest - added new test

--- tst/io/bzz/xo/model/FieldTest.java
+++ tst/io/bzz/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.bzz.xo.model;
 
+import io.bzz.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
@@ -24,5 +25,56 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure, actualFigure);
+    }
+
+    @Test
+    public void testGetFigureWhenFigureIsNotSet() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    public void testGetFigureWhenXLessThenZero() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
+        try {
+            field.getFigure(inputPoint);
+            fail();
+              } catch (InvalidPointException e) {}
+
+    }
+    @Test
+    public void testGetFigureWhenYLessThenZero() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-1);
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {}
+
+    }
+    @Test
+    public void testGetFigureWhenXMoreThenSize() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(5,0);
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {}
+
+    }
+    @Test
+    public void testGetFigureWhenYMoreThenSize() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,5);
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {}
+
     }
 }
