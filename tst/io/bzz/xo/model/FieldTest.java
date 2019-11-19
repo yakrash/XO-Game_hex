a27b3e42d947a8ea99c5ca408f69f166649d19ac
yakrash <yakrash@ya.ru>
2019-11-19 14:05:46 +0300

    FieldTest - created

---
+++ tst/io/bzz/xo/model/FieldTest.java
@@ -0,0 +1,28 @@
+package io.bzz.xo.model;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+
+    @Test
+    public void getSize() {
+        final Field field = new Field();
+        assertEquals(3, field.getSize());
+    }
+
+    @Test
+    public void setFigure() {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(inputFigure, actualFigure);
+    }
+}
