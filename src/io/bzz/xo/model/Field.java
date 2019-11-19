2d41f67a7f90044763050b2b364e8d77f15c23ef
yakrash <yakrash@ya.ru>
2019-11-19 15:00:20 +0300

    Field - added exception point

--- src/io/bzz/xo/model/Field.java
+++ src/io/bzz/xo/model/Field.java
@@ -1,10 +1,15 @@
 package io.bzz.xo.model;
 
+import io.bzz.xo.model.exceptions.AlreadyOccupiedException;
+import io.bzz.xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
 
     private final static int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
     private final Figure [][] field = new Figure [FIELD_SIZE][FIELD_SIZE];
 
 
@@ -12,11 +17,34 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure (final Point point) {
+    public Figure getFigure (final Point point)
+            throws InvalidPointException {
+
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure (final Point point, final Figure figure){
+    public void setFigure (final Point point, final Figure figure)
+            throws InvalidPointException, AlreadyOccupiedException {
+
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
+
+        if (getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+
         field[point.x][point.y] = figure;
+    }
+
+    public boolean checkPoint(final Point point){
+        return checkCoordinate(point.x) && checkCoordinate(point.x);
+    }
+
+    public boolean checkCoordinate (int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
