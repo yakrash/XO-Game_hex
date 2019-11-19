b244ba82831f9cb0bdfde4de5d2069d35cf863cc
yakrash <yakrash@ya.ru>
2019-11-19 18:22:57 +0300

    moveController - created

---
+++ src/io/bzz/xo/controllers/moveController.java
@@ -0,0 +1,23 @@
+package io.bzz.xo.controllers;
+
+import io.bzz.xo.model.Field;
+import io.bzz.xo.model.Figure;
+import io.bzz.xo.model.exceptions.AlreadyOccupiedException;
+import io.bzz.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class moveController {
+
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure)
+            throws InvalidPointException, AlreadyOccupiedException {
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+
+        field.setFigure(point, figure);
+    }
+
+}
