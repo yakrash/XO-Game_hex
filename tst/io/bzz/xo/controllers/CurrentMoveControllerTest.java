6240f54873f267bad58f5a2ac043f627dd8cc17f
yakrash <yakrash@ya.ru>
2019-11-20 11:22:48 +0300

    CurrentMoveControllerTest - created

--- tst/io/bzz/xo/controllers/CurrentMoveControllerTest.java
+++ tst/io/bzz/xo/controllers/CurrentMoveControllerTest.java
@@ -1,15 +1,51 @@
 package io.bzz.xo.controllers;
 
 import io.bzz.xo.model.Field;
+import io.bzz.xo.model.Figure;
+import io.bzz.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
+
+import java.awt.*;
 
 import static org.junit.Assert.*;
 
 public class CurrentMoveControllerTest {
 
     @Test
-    public void currentMove() {
-        Field field = new Field();
-
+    public void testCurrentMoveControllerWhenDraw() throws InvalidPointException {
+        final Field field = new Field();
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        for (int i = 0; i < field.getSize(); i++) {
+            field.setFigure(new Point(0, i), Figure.X);
+            field.setFigure(new Point(1, i), Figure.X);
+            field.setFigure(new Point(2, i), Figure.X);
+            }
+        assertNull(currentMoveController.currentMove(field));
     }
+
+    @Test
+    public void testCurrentMoveControllerWhenGoX() throws InvalidPointException {
+        final Field field = new Field();
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+            field.setFigure(new Point(0, 0), Figure.X);
+            field.setFigure(new Point(1, 1), Figure.O);
+            field.setFigure(new Point(2, 2), Figure.X);
+            field.setFigure(new Point(1, 2), Figure.O);
+
+        assertEquals(Figure.X, currentMoveController.currentMove(field));
+    }
+
+    @Test
+    public void testCurrentMoveControllerWhenGoO() throws InvalidPointException {
+        final Field field = new Field();
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.O);
+        field.setFigure(new Point(2, 2), Figure.X);
+        field.setFigure(new Point(1, 2), Figure.O);
+        field.setFigure(new Point(0, 2), Figure.X);
+
+        assertEquals(Figure.O, currentMoveController.currentMove(field));
+    }
+
 }
